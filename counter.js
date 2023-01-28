0fd9bd092d5a61e928aada4fe1753dae393d7a82
Jakobi Haskell <jakobi_haskell@brown.edu>
2023-01-28 12:06:14 -0500

    counter.js

--- counter.js
+++ counter.js
@@ -1,9 +1,16 @@
-export function setupCounter(element) {
-  let counter = 0
-  const setCounter = (count) => {
-    counter = count
-    element.innerHTML = `count is ${counter}`
-  }
-  element.addEventListener('click', () => setCounter(counter + 1))
-  setCounter(0)
-}
+AFRAME.registerComponent('markerhandler', {
+
+  init: function() {
+      const animatedMarker = document.querySelector("#animated-marker");
+      const aEntity = document.querySelector("#animated-model");
+
+      // every click, we make our model grow in size :)
+      animatedMarker.addEventListener('click', function(ev, target){
+          const intersectedElement = ev && ev.detail && ev.detail.intersectedEl;
+          if (aEntity && intersectedElement === aEntity) {
+              const scale = aEntity.getAttribute('scale');
+              Object.keys(scale).forEach((key) => scale[key] = scale[key] + 1);
+              aEntity.setAttribute('scale', scale);
+          }
+      });
+}});
